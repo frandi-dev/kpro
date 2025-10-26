804bce66c8ef8591aa98cbba2ffd9c7cb241c2aa
frandi-dev <frandipradana31@gmail.com>
2025-10-26 17:20:28 +0700

    membuat spash scren

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./page/Login";
 import AdminLayout from "./component/AdminLayout";
 import Rooms from "./page/Rooms";
+import Spash from "./page/Spash";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Spash />} />
+      <Route path="/login" element={<Login />} />
       {/* role kasir /  resepsionis / dapur */}
       <Route path="/staff">
         <Route index element={<h1>Staff</h1>} />
