e86f12ef5f7a0cb514645a5a66779c98f8ffd361
frandi-dev <frandipradana31@gmail.com>
2025-10-27 03:38:58 +0700

    delete fitur crud room

--- src/component/RoomChard.jsx
+++ src/component/RoomChard.jsx
@@ -1,25 +1,6 @@
-import { useToest } from "../context/ToestContext";
-import { useAlert } from "../context/AlertContext";
-import api from "../lib/api";
-import useConfig from "../lib/config";
 import idr from "../lib/Idr";
 
-const RoomChard = ({
-  name,
-  time,
-  price,
-  customer,
-  status,
-  setLoading,
-  setEditData,
-  action,
-}) => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  const role = user.role;
-  const toest = useToest();
-  const alert = useAlert();
-  const { api_url } = useConfig();
-
+const RoomChard = ({ name, time, price, customer, status }) => {
   const renderButtonStatus = () => {
     if (status === "available") {
       return (
@@ -60,57 +41,11 @@ const RoomChard = ({
     );
   };
 
-  const handleDelete = async () => {
-    setLoading(true);
-    try {
-      await toest.message("Press 'Yes' to continue");
-      const res = await api.delete(`${api_url}/rooms/${name}`);
-      const result = await res.json();
-      alert.success(result.message);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      console.info(error);
-    }
-  };
-
-  const hadleEdit = (e) => {
-    e.preventDefault();
-    setEditData({
-      name,
-      price,
-    });
-    action(true);
-  };
-
   return (
     <div className="col-2" style={{ padding: "8px" }}>
       <div className="card">
         <div className="card-body">
-          <h4 className="card-title">{name}</h4>
-          {role === "admin" && (
-            <div
-              className="action position-absolute top-0 end-0 pt-3"
-              style={{ justifyContent: "center", alignItems: "center" }}
-            >
-              <button
-                className="badge text-bg-primary"
-                style={{ border: "none", marginRight: 4 }}
-                data-bs-toggle="modal"
-                data-bs-target="#formModal"
-                onClick={hadleEdit}
-              >
-                Edit
-              </button>
-              <button
-                className="badge text-bg-danger"
-                style={{ border: "none", marginRight: 8 }}
-                onClick={handleDelete}
-              >
-                Delete
-              </button>
-            </div>
-          )}
+          <h4 className="card-title">{name.toUpperCase()}</h4>
 
           <h6 className="card-subtitle mb-2 text-body-secondary">
             Price/H : {idr(price)}
--- src/page/Rooms.jsx
+++ src/page/Rooms.jsx
@@ -1,121 +1,53 @@
 import RoomChard from "../component/RoomChard";
-import { FaPlus } from "react-icons/fa6";
-import Modal from "../component/Modal";
-import RoomForm from "../component/RoomForm";
 import { useEffect, useState } from "react";
 import useConfig from "../lib/config";
 import api from "../lib/api";
 import { useAlert } from "../context/AlertContext";
+import Spiner from "../component/Spiner";
 
 const Rooms = () => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  const role = user.role;
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState(0);
   const [rooms, setRooms] = useState();
   const { api_url } = useConfig();
   const message = useAlert();
   const [loading, setLoading] = useState(false);
-  const [editAction, setEditAction] = useState(false);
-  const [editData, setEditData] = useState({});
 
   // fetch rooms
   useEffect(() => {
     const fetchRoom = async () => {
+      setLoading(true);
       const res = await api.get(`${api_url}/rooms`);
       const result = await res.json();
       if (result.error) {
         message.error(result.message);
       } else {
         setRooms(result.data);
+        setLoading(false);
       }
     };
     fetchRoom();
   }, [loading, setLoading]);
 
-  useEffect(() => {
-    setName(editData.name);
-    setPrice(editData.price);
-  }, [editData, setEditData]);
-
-  // handle add room
-  const handleSubmitCreate = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    try {
-      const res = await api.post(`${api_url}/rooms`, {
-        name: name,
-        price_per_hour: price,
-      });
-      const result = await res.json();
-      if (res.status === 201) {
-        message.success(result.message);
-        setLoading(false);
-      } else {
-        message.error(result.message);
-      }
-    } catch (error) {
-      message.error(error.message);
-    }
-  };
-
-  const handleSubmitEdit = (e) => {
-    e.preventDefault();
-    // setLoading(true);
-    console.log(name);
-    console.log(price);
-  };
-
-  const handleSubmit =
-    editAction === true ? handleSubmitEdit : handleSubmitCreate;
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <Modal
-          title={editAction ? "Edit room" : "Create room"}
-          action={setEditAction}
-        >
-          <RoomForm
-            name={name ?? ""}
-            price={price ?? 0}
-            setName={setName}
-            setPrice={setPrice}
-          />
-        </Modal>
-      </form>
-      <div className="row p-2">
-        {role === "admin" && (
-          <div className="col-12 mb-4">
-            <button
-              className="btn btn-primary"
-              data-bs-toggle="modal"
-              data-bs-target="#formModal"
-              onClick={() => {
-                setName("");
-                setPrice(0);
-                setEditAction(false);
-                setEditData({});
-              }}
-            >
-              <FaPlus style={{ marginBottom: "2px" }} /> New
-            </button>
-          </div>
-        )}
-
-        {rooms === undefined
-          ? "Not Found!"
-          : rooms.map((room) => (
-              <RoomChard
-                key={room.id}
-                name={room.name}
-                status={room.status}
-                price={room.price_per_hour}
-                setLoading={setLoading}
-                action={setEditAction}
-                setEditData={setEditData}
-              />
-            ))}
-      </div>
+      {loading ? (
+        <div className="d-flex justify-content-center mt-4">
+          <Spiner />
+        </div>
+      ) : (
+        <div className="row p-2">
+          {rooms === undefined
+            ? "Not Found!"
+            : rooms.map((room) => (
+                <RoomChard
+                  key={room.id}
+                  name={room.name}
+                  status={room.status}
+                  price={room.price_per_hour}
+                  setLoading={setLoading}
+                />
+              ))}
+        </div>
+      )}
     </>
   );
 };
